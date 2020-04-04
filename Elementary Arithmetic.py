30e7a8f1e4abd3696e38c3aa384651e42eb1aa07
Ragib Shahariar <ragib.ayon@live.com>
2020-04-04 19:27:20 +0600

    github desktop

---
+++ Elementary Arithmetic.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 23:00:18 2020
+Elementary Arithmetic
+@author: Ragib Shahariar Ayon
+"""
+
+a,b=input().split()
+print(int(a)+int(b))
--- temp.py
+++ temp.py
@@ -1,30 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Mar 21 20:25:40 2020
-3
-1
-10
-3
-1 2 3
-9
-200 933 100 6 20 103 310 1 143
-@author: Ragib Shahariar Ayon
-"""
+a=5
+b=6
 
-
-def bounty_count(prisoner_bounty=0,prisonar_count=0):
-    bounty_sum=0
-    for bounty in prisoner_bounty:
-        bounty_sum+=int(bounty)
-        total_bounty=(1/prisonar_count)*bounty_sum
-    return total_bounty
-    
-
-
-test_case=int(input())
-
-for i in range(test_case):
-    prisonar_count=int(input())
-    prisoner_bounty=input().split()
-    print(bounty_count(prisoner_bounty,prisonar_count))
-
+c=a+b
+print(c)
